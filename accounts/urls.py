eb01e24d2e9d6d01ae44a2c00215d251983508f4
Андрей Асташкин <aastashkin2001@gmail.com>
2026-01-15 15:03:16 +0300

    fix accounts/urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     ),
     path(
         "logout/",
-        auth_views.LogoutView.as_view(),
+        auth_views.LogoutView.as_view(next_page="login"),
         name="logout",
     ),
 
